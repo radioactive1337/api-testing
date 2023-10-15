e347dd97d921e4e8dceeec197538f66fe2e36dde
radioactive1337 <vysotskiidanil@gmail.com>
2023-10-15 19:12:01 +0600

    some improvements

--- api/send_request_api.py
+++ api/send_request_api.py
@@ -1,11 +1,14 @@
+import allure
+
 from api.api import Api
-from data_models.create_user_model import CreateUserModelRequest, CreateUserModelResponse
+from data_models.create_user_model import CreateUserModelRequest
 
 
 class SendrequestApi(Api):
     _URL = "https://send-request.me"
     _ENDPOINT = "/api/users/"
 
+    @allure.step("Creating a user")
     def create_user(self, request_body: CreateUserModelRequest):
         return self.post(url=self._URL,
                          endpoint=self._ENDPOINT,
--- test/user/test_user_creation.py
+++ test/user/test_user_creation.py
@@ -8,10 +8,13 @@ from api.send_request_api import SendrequestApi
 
 @allure.title("Creating a user")
 @pytest.mark.parametrize("req_params", data_loader("user_data", "data"))
-def test_qwe(req_params):
+def test_user_creation(req_params):
+    """
+    trying to create a user ...
+    """
     SendrequestApi().create_user(
         request_body=req_params). \
-        status_code_should_be(2010). \
+        status_code_should_be(201). \
         jsonschema_should_be_valid("one_user_schema"). \
         value_in_response_parameter(["first_name"], req_params.first_name). \
         value_in_response_parameter(["last_name"], req_params.last_name)
