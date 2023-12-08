20b4c4602fe95594019d8299417e7a001af1a34b
radioactive1337 <vysotskiidanil@gmail.com>
2023-12-08 19:24:37 +0600

    some improvements, json handler

---
+++ tools/json_handler.py
@@ -0,0 +1,28 @@
+import json
+
+
+class JsonHandler:
+    def update_json(self, json_data, key, new_value):
+        if isinstance(json_data, dict):
+            for k, v in json_data.items():
+                if k == key:
+                    json_data[k] = new_value
+                elif isinstance(v, dict):
+                    self.update_json(v, key, new_value)
+                elif isinstance(v, list):
+                    for item in v:
+                        if isinstance(item, dict):
+                            self.update_json(item, key, new_value)
+        return json.dumps(json_data)
+
+    def get_value_by_key(self, key, input_dict):
+        if not isinstance(input_dict, dict):
+            return "Error: input object is not a dict"
+        for k, v in input_dict.items():
+            if k == key:
+                return v
+            elif isinstance(v, dict):
+                result = self.get_value_by_key(key, v)
+                if result is not None:
+                    return result
+        return f"Error: Key '{key}' is not found"
